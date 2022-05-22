583d0402831dc375213e340d6814e0d5a41b5ba6
Gary Williams <w_gwilliams@yahoo.com>
2022-05-22 06:20:19 -0500

    tested adding route and more initial setup

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 //Load express package
 const express = require('express');
-
 const PORT = process.env.PORT || 3000;
 
 //Instantiate the server
 const app = express();
 
+const { notes } = require('./Develop/db/db.json')
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 
+//test adding route
+app.get('/api/notes', (req, res) => {
+    res.send('Hello!');
+  });
 
+
+//make our server listen on port 3000
 app.listen(PORT, () => {
     console.log('API server now on port 3000');
 })
